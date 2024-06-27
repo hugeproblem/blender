05edbe46ea585623d2fb7da55b59dfb3b409fc09
hugeproblem <yuguo@yuguo.me>
2024-06-28 00:47:00 +0800

    now able to evaluate expression

--- source/blender/nodes/function/nodes/node_fn_expr_eval.cc
+++ source/blender/nodes/function/nodes/node_fn_expr_eval.cc
@@ -5,6 +5,7 @@
 #include "node_function_util.hh"
 #include "UI_interface.hh"
 #include "UI_resources.hh"
+#include "exprtk.hpp"
 
 namespace blender::nodes::node_fn_expr_eval_cc {
 
@@ -53,13 +54,80 @@ static void node_layout(uiLayout *layout, bContext*, PointerRNA *ptr)
   uiItemR(layout, ptr, "expression", UI_ITEM_NONE, "", ICON_NONE);
 }
 
+class ExprtkEvaluator : public mf::MultiFunction
+{
+public:
+  using Type = float;
+  
+private:
+  exprtk::expression<Type>   expr_;
+  exprtk::symbol_table<Type> vars_;
+  bool is_valid_ = false;
+  mutable Type a = 0, b = 0, c = 0, d = 0;
+
+public:
+  ExprtkEvaluator(char const* expr_str)
+  {
+    vars_.add_constants();
+    vars_.add_variable("a", a);
+    vars_.add_variable("b", b);
+    vars_.add_variable("c", c);
+    vars_.add_variable("d", d);
+    expr_.register_symbol_table(vars_);
+
+    exprtk::parser<Type> parser;
+    if (expr_str)
+      is_valid_ = parser.compile(expr_str, expr_);
+    else
+      is_valid_ = false;
+
+    static const mf::Signature signature = []() {
+      mf::Signature signature;
+      mf::SignatureBuilder builder{"Expression Evaluator", signature};
+      builder.single_input<Type>("a");
+      builder.single_input<Type>("b");
+      builder.single_input<Type>("c");
+      builder.single_input<Type>("d");
+      builder.single_output<Type>("result");
+      return signature;
+    }();
+    this->set_signature(&signature);
+  }
+
+  void call(const IndexMask &mask, mf::Params params, mf::Context /*context*/) const override
+  {
+    const VArray<float> &input_as = params.readonly_single_input<float>(0, "a");
+    const VArray<float> &input_bs = params.readonly_single_input<float>(1, "b");
+    const VArray<float> &input_cs = params.readonly_single_input<float>(2, "c");
+    const VArray<float> &input_ds = params.readonly_single_input<float>(3, "d");
+
+    auto results = params.uninitialized_single_output<float>(4, "result");
+
+    mask.foreach_index([&](const int i) {
+      a = input_as[i];
+      b = input_bs[i];
+      c = input_cs[i];
+      d = input_ds[i];
+      results[i] = expr_.value();
+    });
+  }
+
+  ExecutionHints get_execution_hints() const override
+  {
+    ExecutionHints hints;
+    hints.min_grain_size = 65535;
+    return hints;
+  }
+};
+
 static void node_build_multi_function(NodeMultiFunctionBuilder &builder)
 {
   const bNode &node = builder.node();
-  static auto fn = mf::build::SI4_SO<float, float, float, float, float>("Eval Expr", [](float a, float b, float c, float d){
-      return a+b;
-    });
-  builder.set_matching_fn(fn);
+  //static auto fn = mf::build::SI4_SO<float, float, float, float, float>("Eval Expr", [](float a, float b, float c, float d){
+  //    return a+b;
+  //  });
+  //builder.set_matching_fn(fn);
+  builder.construct_and_set_matching_fn<ExprtkEvaluator>(node_storage(node).expression);
 }
 
 static void node_register()
